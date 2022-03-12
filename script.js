6932903fbe29e17eac21f439541dca176a4ef38d
Lin <josephgarcia9919@gmail.com>
2022-03-11 19:03:52 -0800

    Set the base for rock paper scissors.

---
+++ script.js
@@ -0,0 +1,10 @@
+const computerPlay =() => {
+    let random = Math.floor(Math.random() * 3);
+    if (random === 1){
+        return 'rock';
+    } else if(random === 2){
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
