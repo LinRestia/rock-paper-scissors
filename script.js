14c40e3a458f822f3a97b0ad953e99a6cdfd3df0
Lin <josephgarcia9919@gmail.com>
2022-03-15 17:24:29 -0700

    Add playerSelection and PlayTheGame.

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 const computerPlay =() => {
     let random = Math.floor(Math.random() * 3);
     if (random === 1){
-        console.log('rock');
+        return 'rock';
     } else if(random === 2){
-        console.log('paper');
+        return 'paper';
     } else {
-        console.log('scissors');
+        return 'scissors';
+    }
+}
+const playerSelection = choice => {
+    if (choice.toLowerCase() === 'rock') {
+        return 'rock';
+    } else if (choice.toLowerCase() === 'paper') {
+        return 'paper';
+    } else if (choice.toLowerCase() === 'scissors' || choice.toLowerCase() === 'scissor') {
+        return 'scissors'
+    } else {
+        return 'Please choose between rock,paper, or scissors.'
+    }
+}
+
+const playTheGame = (computerPlay, playerSelection) => {
+    if (playerSelection === 'rock' && computerPlay === 'rock') {
+        return 'It\'s a tie!';
+    } else if (playerSelection === 'rock' && computerPlay ==='paper') {
+        return 'Computer Win';
+    } else if (playerSelection === 'rock' && computerPlay === 'scissors') {
+        return 'Player Win!';
+    }
+    if (playerSelection === 'paper' && computerPlay === 'rock') {
+        return 'Player Win';
+    } else if (playerSelection === 'paper' && computerPlay === 'paper') {
+        return 'It\'s a tie';
+    } else if (playerSelection === 'paper' && computerPlay === 'scissors') {
+        return 'Computer Win';
+    }
+    if (playerSelection === 'scissors' && computerPlay === 'rock') {
+        return 'Computer Win';
+    } else if (playerSelection === 'scissors' && computerPlay ==='paper') {
+        return 'Player Win';
+    } else if (playerSelection === 'scissors' && computerPlay === 'scissors') {
+        return 'It\'s a tie';
     }
 }
