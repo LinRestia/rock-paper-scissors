bbeab9939fd39ae4f0457f9f08dd0230e88b119e
Lin <josephgarcia9919@gmail.com>
2022-04-29 20:30:23 -0700

    This took a while haha.     Im not sure how this will look in the commit messages but     I actually had to stop this project in order to study more     since i could not complete it. Now i came back and have     finished it and i hope it to one day add the details     to this page. I have learned alot from this project!

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
-const computerplay = () => {
-    random = Math.floor(Math.random() * 3);
-    if (random = 0) {
-        return 'rock';
-    } else if (random = 1) {
-        return 'paper';
-    } else if (random = 2) {
-        return 'scissors';
-    } else {
-        return 'invalid';
+let playerWin = 0;
+let computerWin = 0;
+const computerPlays = () => {
+    gen = Math.floor(Math.random() * 3);
+    const random = {
+        0: 'rock',
+        1: 'scissors',
+        2: 'paper'
     }
+    return random[gen];
 }
-
-const oneRound = (playerSelection, computerSelection) => {
-    if (playerSelection === 'rock' && computerSelection ==='paper') {
-        return 'You Lose :(';
-    } else if (playerSelection === 'rock' && computerSelection ==='scissors') {
-        return 'You Win :)';
-    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        return 'Tie!' 
-    } else if (playerSelection ==='paper' && computerSelection === 'scissors') {
-        return 'You Lose :(';
+const oneGame = () => {
+    const comp = 'Computer: ';
+    const player = 'Player: '
+    computerSelection = computerPlays();
+    if (playerSelection === computerSelection) {
+        console.log('Its a Draw!');
+        return 'Draw';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerWin++;
+        console.log('You Lose :(');
+        console.log(comp + computerWin);
+        return 'Loss';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerWin++
+        console.log('You Win :)');
+        console.log(player + playerWin);
+        return 'Win';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerWin++;
+        console.log('You Lose :(');
+        console.log(comp + computerWin);
+        return 'Lose';
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Win :)';
-    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'Tie!';
-    } else if (playerSelection === 'scissors' && computerSelection ==='rock') {
-        return 'You Lose :(';
+        playerWin++;
+        console.log('You Win :)');
+        console.log(player + playerWin);
+        return 'Win';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerWin++;
+        console.log('You Lose :(');
+        console.log(comp + computerWin);
+        return 'Lose';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You Win :)';
-    } else if (playerSelection ==='scissors' && computerSelection ==='scissors') {
-        return 'Tie!';
+        playerWin++;
+        console.log('You Win :)');
+        console.log(player + playerWin);
+        return 'Win';
     }
 }
-
-const playerSelection = "paper";
-const computerSelection = computerplay();
-console.log(oneRound(playerSelection, computerSelection));
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        oneGame();
+    } if (playerWin > computerWin) {
+        console.log('It\'s the player\'s Win!')
+    } else if (playerWin === computerWin) {
+        console.log('Its a Draw :o');
+    } else if (playerWin < computerWin) {
+        console.log('Its the Computers\'s Win!');
+    }
+    return 'end';
+}
+const playerSelection = prompt('choose between rock, paper and scissors');
